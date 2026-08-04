8105e14a87357012858ef9bd801e30b14c50e555
Raj Bhosale <raj43bhosale23@gmail.com>
2026-08-04 16:11:59 +0530

    Time: 491 ms (11.01%) | Memory: 16.5 MB (5.45%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,10 +3,9 @@ class Solution(object):
         i=0
         op = []
         m = 0
-        while i<len(s):
+        for i in range(len(s)):
             if s[i] in op:
                 m = max(m , len(op))
                 op = op[op.index(s[i])+1:]
             op.append(s[i])
-            i+=1
         return max(m, len(op))
